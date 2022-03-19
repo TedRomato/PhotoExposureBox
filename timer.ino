3b83c5b0123e5f9ade47e2ac5395575a8b3fb6e5
TedRomato <59472129+TedRomato@users.noreply.github.com>
2022-03-19 22:27:05 +0100

    brightness change

--- timer.ino
+++ timer.ino
@@ -3,29 +3,33 @@
 
 
 
-
 const int timerPotPin = 0;
+const int brightnessPotPin = 1;
 const int minReadableVal = 50;
 const int maxReadableVal = 1000;
 const int potNoiseThreshold = 10;
-AnalogReader analogReader(potNoiseThreshold,5,timerPotPin,minReadableVal,maxReadableVal);
+AnalogReader timerAnalogReader(0,1023,potNoiseThreshold,timerPotPin,minReadableVal,maxReadableVal);
+AnalogReader brightnessAnalogReader(0,255,potNoiseThreshold,brightnessPotPin,minReadableVal,maxReadableVal);
+
 
 const int rs = 13, en = 12, d4 = 11, d5 = 10, d6 = 9, d7 = 8;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-const int ledPin = 7;
-const int pushButtonPin = 6;
+
+const int ledPin = 6;
+const int pushButtonPin = 7;
 
  
 const int timerTurnJump = 15; 
 // min and max timer values in seconds (maxTimerVal - mibTimerVal) must be divisible by timerTurnJump
 const int minTimerVal = 120; 
-const int maxTimerVal = 1200;
+const int maxTimerVal = 1800;
 
 const int possibleTimes = ( maxTimerVal - minTimerVal ) / timerTurnJump;
 
 
 int timer;
+long prevTimer = 0;
 const int delaySecond = 500; // delay value to wait one second in milisecond
 
 int state = 0; /*POSSIBLE STATES: 
@@ -36,14 +40,17 @@ int state = 0; /*POSSIBLE STATES:
 
 
 void setup() {
+  Serial.begin(9600);
   // set up pin for turning on LEDs via relay
   pinMode(ledPin, OUTPUT);
   // set up pin for push button 
   pinMode(pushButtonPin, INPUT);
   // turn off LEDs
-  powerLEDs(false);
+  analogWrite(ledPin, 0);
   //set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
+  timerAnalogReader.setOutputRange(0, possibleTimes);
+
 }
 
 void loop() {
@@ -51,7 +58,7 @@ void loop() {
   if(state == 0) {
     
     // update timer display if new avlue is read from pot
-    boolean differentValueRead = analogReader.update();
+    boolean differentValueRead = timerAnalogReader.update();
     if(differentValueRead) {
       updateTimerValue();
       displayTimer("Timer: ", timer);
@@ -60,23 +67,33 @@ void loop() {
     // start the timer and turn on LEDs on button press
     if(pushButtonPressed()) {
       state = 1;
-      powerLEDs(true);
+      brightnessAnalogReader.update();
+      analogWrite(ledPin, brightnessAnalogReader.getCurrentValue());
       displayTimer("Time left: ", timer);
+      delay(delaySecond);
     }
   }
 
   // Timer running
   else if(state == 1) {
+
+    // update brightness
+    if(brightnessAnalogReader.update()){
+      analogWrite(ledPin, brightnessAnalogReader.getCurrentValue());  
+    }
     
     //wait one second
-    delay(delaySecond);
-    timer -= 1;
-
+    long currTimer = millis();
+    if(currTimer - prevTimer > 1000){
+      timer -= 1;
+      displayTimer("Time left: ", timer);
+      prevTimer = currTimer;
+    }
     // when timer reaches zero, turn off LEDs move to state 2 and display timer finished
 
     if (timer <= 0) {
 
-      powerLEDs(false);
+      analogWrite(ledPin, 0);
       showLabel("Timer Over!");
       state = 2; 
       delay(delaySecond);
@@ -84,22 +101,18 @@ void loop() {
     // when push button (cancel) is pressed, turn off LEDs move to state 2 and display timer canceled
     } else if (pushButtonPressed()) {
 
-      powerLEDs(false);
+      analogWrite(ledPin, 0);
       showLabel("Timer Canceled!");
       state = 2; 
       delay(delaySecond);
 
     // otherwise display current time
-    } else {
-      
-      displayTimer("Time left: ", timer);
-    
     }
   }
 
   else if(state == 2) {
     // when timer pot gets moved move to state 0
-    if(analogReader.update()) {
+    if(timerAnalogReader.update()) {
       
       state = 0;
       
@@ -108,7 +121,7 @@ void loop() {
     else if(pushButtonPressed()) { 
 
       // displaying timer
-      analogReader.update();
+      timerAnalogReader.update();
       updateTimerValue();
       displayTimer("Timer: ", timer);
 
@@ -134,7 +147,7 @@ void loop() {
 
 // map analog value to correct time
 void updateTimerValue() {
-  timer = minTimerVal + timerTurnJump*analogReader.getCurrentValueMapped(possibleTimes, 0);
+  timer = minTimerVal + timerTurnJump*timerAnalogReader.getCurrentValue();
 }
 
 // displays custom label on first row, and time in format "x min y sec" on second row
@@ -166,11 +179,3 @@ boolean pushButtonPressed() {
 }
 
 
-void powerLEDs(boolean power) {
-  if(power) {
-    digitalWrite(ledPin, LOW);
-  }
-  else {
-    digitalWrite(ledPin, HIGH);
-  }
-}
